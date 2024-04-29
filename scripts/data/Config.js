c3094e0729d3d20f23116cb89fc8eeb1b3060459
ENIACJushi <102369697+ENIACJushi@users.noreply.github.com>
2024-04-29 22:38:09 +0800

    AFK

--- scripts/data/Config.js
+++ scripts/data/Config.js
@@ -1,11 +1,12 @@
 export const Config = {
-    ITEMMAX: 100,
-    flyArea: [
+    ITEMMAX: 100, // 掉落物清理阈值
+    flyArea: [ // 生存飞行区
         {
             "name": "",
             "dimension": "minecraft:overworld",
             "start": [-6000, -6000],
             "end": [6000, 6000]
         }
-    ]
+    ],
+    AFKTime: 120 // AFK等待时间 秒
 }
--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,16 @@ import { GameMode, Player, system, world, ScriptEventCommandMessageAfterEvent } 
 import * as Tool from "./libs/Tools"
 import * as Clean from "./doge/Clean"
 import * as Fly from "./doge/Fly"
+import * as AFK from "./doge/AFK";
 
 // Script Event
 system.afterEvents.scriptEventReceive.subscribe(event => {
     system.run(()=>{
         switch(event.id){
-            case "doge:clean": Clean.startClean(event); break;
-            case "doge:help": break;
-            default: break;
+            case "doge:clean": Clean.startClean(event); break; // 立即开始清理掉落物
+            case "doge:afk": AFK.setAFK(event.sourceEntity); break; // 令玩家立即进入AFK状态
+            case "doge:noafk": event.sourceEntity.addTag("NOAFK"); break; // 令玩家不会被AFK检测
+            default: event.sourceEntity.sendMessage(`doge:clean, doge:afk, doge:noafk`); break;
         }
     })
 }, {namespaces: ["doge"]});
@@ -19,5 +21,6 @@ world.afterEvents.playerSpawn.subscribe(event => {
     // 进服事件
     if(event.initialSpawn){
         Fly.playerJoinEvent(event.player);
+        AFK.reset();
     }
 });
